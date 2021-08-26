bfef7d77b912d94dbeb2bc1813d6a9f0ec5bd26a
michael <michaelmin2020@gmail.com>
2021-08-26 17:51:16 +0200

    function version of this project

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,21 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+#from flask_bootstrap import Bootstrap
+#from flask_migrate import Migrate
 
 db = SQLAlchemy()
+bootstrap = Bootstrap()
 # migrate = Migrate()
 
 
-def create_app(config_type):
+
+def create_app(config_type): #dev, test, or prod
     app = Flask(__name__)
     configuration = os.path.join(os.getcwd(), 'config', config_type + '.py')
     app.config.from_pyfile(configuration)
-    db.init_app(app)
+    db.init_app(app) # bind database to flask app
+    #bootstrap.init_app(app) # initialize boostrap
     # migrate.init_app(app, db)
 
     from app.catalog import main
--- app/catalog/routes.py
+++ app/catalog/routes.py
@@ -1,6 +1,6 @@
 from app.catalog import main
-from app.catalog.models import Book, Publication
 from app import db
+from app.catalog.models import Book, Publication
 from flask import render_template
 
 
@@ -8,3 +8,12 @@ from flask import render_template
 def display_books():
     books = Book.query.all()
     return render_template('home.html', books=books)
+
+
+#@main.route('/display/publisher/<publisher_id>')
+@main.route('/display/publisher/')
+def display_publisher(publisher_id):
+    publisher = Publication.query.filter_by(id=publisher_id).first()
+    publisher_books = Book.query.filter_by(pub_id = publisher.id).all()
+
+    return render_template('publisher.html', publisher=publisher, publisher_books=publisher_books)
